1e9d5bf00036e0f490ef7ca4e28876f6e5ec2218
Neil Mastroianni <nmastroianni@longbranch.k12.nj.us>
2022-07-06 14:59:49 -0400

    adding dated directories

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
+const fs = require("fs");
 const puppeteer = require("puppeteer");
 const sites = require("./sites");
 
+const date = new Date();
+const monthNumber = date.getMonth() + 1;
+const dateNumber = date.getDate();
+const year = date.getFullYear();
+const directoryName = `${year}_${monthNumber}_${dateNumber}`;
+
 sites.forEach(async ({ url, name }) => {
+  /**
+   * Check to see if images directory is present
+   * If not, create it.
+   */
+  fs.access("./images", (error) => {
+    error && fs.mkdirSync("./images");
+  });
+
+  /**
+   * Check to see if a directory with today's date exists
+   * If not, create it.
+   */
+  fs.access(`./images/${directoryName}`, (error) => {
+    error && fs.mkdirSync(`./images/${directoryName}`, { recursive: true });
+  });
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  await page.screenshot({ path: `./images/image-${name}.png` });
+  await page.screenshot({
+    path: `./images/${directoryName}/image-${name}.png`,
+  });
   await browser.close();
 });
